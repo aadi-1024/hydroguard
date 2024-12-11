d3b298d54fcc3a383560c915667a7f84b8093d72
Aaditya <thakuraaditya1024@gmail.com>
2024-12-11 14:20:22 +0530

    population

--- gee/app.py
+++ gee/app.py
@@ -28,3 +28,31 @@ def get_surf_area():
     return jsonify({
         'cover': str(area_km2)
     })
+
+@app.post('/population')
+def pop():
+    states = ee.FeatureCollection("FAO/GAUL/2015/level1")
+
+
+    data = request.get_json(force=True)
+    state = data['state']
+    year = None
+    try:
+        year = data['year']
+    except:
+        year = '2020'
+
+    population_dataset = ee.ImageCollection("WorldPop/GP/100m/pop").filterDate(f'{year}-01-01', f'{year}-12-31')
+    population_image = population_dataset.mean()
+
+    indiaStates = states.filter(ee.filter.Filter.eq('ADM0_NAME', 'India'))
+    punjabGeo = indiaStates.filter(ee.filter.Filter.eq('ADM1_NAME', state)).geometry()
+
+    total_population = population_image.reduceRegion(
+        reducer=ee.Reducer.sum(),
+        geometry=punjabGeo,
+        scale=100,
+        maxPixels=1e9
+    ).get('population')
+
+    return str(ee.Number(total_population).getInfo())
