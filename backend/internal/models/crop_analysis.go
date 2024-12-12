053da2c13e5262723a47a35f2165bd84eb744a7e
Aaditya <thakuraaditya1024@gmail.com>
2024-12-12 15:38:49 +0530

    idk

--- backend/internal/models/crop_analysis.go
+++ backend/internal/models/crop_analysis.go
@@ -8,10 +8,10 @@ type CropAnalysis struct {
 	CreatedAt            time.Time `json:"created_at"`
 	CropWaterRequirement float64   `json:"crop_water_requirement"`
 	WaterGivenConfig     float64   `json:"water_given_config"`
-	SeepageLosses        float64   `json:"seepage_losses"`
-	EvaporationDischarge float64   `json:"evaporation_discharge"`
-	CanalLosses          float64   `json:"canal_losses"`
-	OptimalWaterUsage    float64   `json:"optimal_water_usage"`
-	Suggestions          string    `json:"suggestions"`
-	ConfigErrors         string    `json:"config_errors"`
+	// SeepageLosses        float64   `json:"seepage_losses"`
+	// EvaporationDischarge float64   `json:"evaporation_discharge"`
+	// CanalLosses          float64   `json:"canal_losses"`
+	OptimalWaterUsage float64 `json:"optimal_water_usage"`
+	Suggestions       string  `json:"suggestions"`
+	ConfigErrors      string  `json:"config_errors"`
 }
